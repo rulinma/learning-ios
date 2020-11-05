44315debd8db9c45eeeb36aef7bcd20acee751d8
rulinma <rulinma@gmail.com>
2020-11-05 22:43:19 +0800

    tableview

---
+++ learning-ios/learning-ios/MyTableViewController.swift
@@ -0,0 +1,60 @@
+//
+//  MyTableViewController.swift
+//  learning-ios
+//
+//  Created by Rollin on 2020/11/5.
+//  Copyright © 2020年 rulinma. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MyTableViewController : UITableViewController {
+    
+    // init
+    override func viewDidLoad() {
+        NSLog("start");
+        
+        // tableview
+        
+        super.viewDidLoad()
+   
+    }
+ 
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        NSLog(String(indexPath.row));
+        NSLog(String(indexPath.section));
+        NSLog(String(tableView.numberOfSections));
+    }
+}
+
+extension MyTableViewController {
+//    override func tableView(_ tableView: UITableView,
+//                            numberOfRowsInSection section: Int) -> Int
+//    {
+//        NSLog("rows");
+//        return 12;
+//    }
+//
+//    override func tableView(_ tableView: UITableView,
+//                            cellForRowAt indexPath: IndexPath) -> UITableViewCell
+//    {
+//        NSLog("cell %d", indexPath.row );
+//
+//        let cell = UITableViewCell();
+//        cell.backgroundColor = UIColor(named: "red");
+//        let label = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 21));
+//        label.text = "Hello World: " + String(indexPath.row) ;
+//        // label.textColor = UIColor(red: 0.05, green: 0.11, blue: 0.24, alpha: 1.00);
+//        cell.addSubview(label);
+//        // cell.backgroundColor = UIColor(red: 0.05, green: 0.11, blue: 0.24, alpha: 1.00);
+//        if ((indexPath.row % 2) == 0) {
+//            cell.backgroundColor =  UIColor.blue;
+//        }
+//
+//        return cell;
+//    }
+
+   
+}
+
--- learning-ios/learning-ios/ViewController.swift
+++ learning-ios/learning-ios/ViewController.swift
@@ -14,6 +14,15 @@ class ViewController: UIViewController {
         NSLog("start");
         
         // 测试 init
+        // hello()
+        
+        // tableview
+        
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    func hello() {
         let p = Person.init(name: "mrl", age: 40);
         NSLog(p.name);
         
@@ -27,11 +36,11 @@ class ViewController: UIViewController {
         let w = "\(width)"
         let height = self.view.frame.height
         let h = "\(height)"
-
+        
         NSLog("width: %@", w);
         NSLog("height: %@", h);
         NSLog("width: %f height: %f", width, height);
-  
+        
         // add hello world
         let label = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 21));
         label.center = CGPoint(x: self.view.frame.width/2, y: self.view.frame.height/2);
@@ -42,11 +51,7 @@ class ViewController: UIViewController {
         self.view.addSubview(label);
         
         self.view.backgroundColor = UIColor(displayP3Red: 1.0, green: 0.0, blue: 0.0, alpha: 1.0);
-        
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
 
 }
 
